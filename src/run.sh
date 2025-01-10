989d309acdef7af0f43cd183220a0861fd3054ba
Woodrow N. Wilson <65731874+potus28@users.noreply.github.com>
2025-01-10 10:06:21 -0600

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,16 +1,18 @@
-
+#!/bin/bash
 
 
 startframe=0
 endframe=5001
+logfreq=100
+
+trajectory=../lammps_example/prod_control.traj
+reference_mol=../lammps_example/spc.xyz
+atom_indices=../lammps_example/all_atom_indices.npy
 
 timestep=0.004 
-logfreq=100
 sigma=2
 temperature=298.0
 eMD=0.0
 
-python decompose_velocities.py ../lammps_example/prod_control.traj $logfreq ../../all_atom_indicies.npy $startframe  $endframe
-python two_phase_dos.py ../lammps_example/prod_control.traj $timestep $sigma ../../spc.xyz $temperature $eMD
-
-
+python decompose_velocities.py $trajectory $logfreq $atom_indices $startframe  $endframe
+python two_phase_dos.py $trajectory $timestep $sigma $reference_mol $temperature $eMD
